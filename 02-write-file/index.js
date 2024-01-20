598307502317957c300fb6647829b6728d6e4c6c
OlgaIndykova <ola1234567890dd@gmail.com>
2024-01-20 12:58:24 +0200

    feat: develop a program that reads your console input until the process is forcibly terminated with ctrl + c

---
+++ 02-write-file/index.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const path = require("path");
+const { stdin, stdout } = process;
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+stdout.write("Welcome to customer support! Please, describe your problem\n");
+
+fs.writeFile(
+  path.join(__dirname, "text.txt"),
+  "",
+  (err) => {
+    if (err) throw err;
+  },
+);
+
+stdin.on("data", (data) => {
+    fs.appendFile(
+      path.join(__dirname, "text.txt"),
+      data.toString(),
+      (err) => {
+        if (err) throw err;
+      },
+    );
+})
+// stdin.on("data", (data) => {
+//   if (data.toString() === "exit") {
+//       emitter.emit(handle());
+//     };
+//   console.log("error");
+// })
+function handle() {
+  console.log("Thank you for your request, our experts will contact you shortly.");
+  process.exit();
+}
+process.on('SIGINT', handle);
+
+emitter.on('exit', handle);
+
+
