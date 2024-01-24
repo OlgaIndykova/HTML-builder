df4394e538954047eea24624360aa589b2452d2d
OlgaIndykova <ola1234567890dd@gmail.com>
2024-01-24 15:12:09 +0200

    feat: solve 1 task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,9 @@
 const path = require("path");
 const fs = require("fs");
-fs.readFile(
-    path.join(__dirname, "text.txt"),
-    "utf-8",
-    (err, data) => {
-      if (err) throw err;
-      console.log(data);
-    },
-  );
+
+const readFileText = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf8');
+
+readFileText.on('data', (data) => {
+  console.log(data);
+})
+
